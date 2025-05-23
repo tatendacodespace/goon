b7a0b81065af4158e435f387be12007a4dc5c785
Tatenda <tatendamidzi49@gmail.com>
2025-05-23 19:08:52 +0200

    refactor: restructure PostCSS and Tailwind configuration files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+  },
+  css: {
+    postcss: './postcss.config.cjs',
   },
   server: {
     port: 3000
